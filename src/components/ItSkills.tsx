9963303b69ad1f4e184f8fabb211e8ae8ddc85eb
Raftouch <rafanelkin@gmail.com>
2023-03-09 12:45:51 +0100

    :art: improved code

--- src/components/ItSkills.tsx
+++ src/components/ItSkills.tsx
@@ -9,7 +9,7 @@ export function ItSkills({it}: ItSkillsProps) {
   return (
     <div className='group'>
       <p className='absolute -translate-x-0 translate-y-[-100%] bg-black/20 text-white rounded px-1 cursor-auto text-sm hidden group-hover:block'>{it.title}</p>
-      <img className='relative w-12 flex items-center' src={it.url} alt={it.title} />
+      <img className='relative h-12' src={it.url} alt={it.title} />
     </div>
   )
 }

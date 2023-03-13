95b4e1e3386e6612760624fd687c96b5682bb5a9
Raftouch <rafanelkin@gmail.com>
2023-03-13 12:54:14 +0100

    :art: modified code

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -7,17 +7,14 @@ import { Contact } from './Contact'
 export function Resume() {
   return (
     <div className='w-full h-full mt-14 bg-black/5'>
-        <div className='flex gap-11 items-center ml-5 pt-10 flex-wrap'>
-            <img className='max-w-[200px] rounded-full' src={cvpic} alt='' />
-            <div className='max-w-[300px] flex flex-col gap-2'>
-                <p className='text-sm'>Nelli Timorshina</p>
-                <h1 className='uppercase font-bold'>Développeuse web</h1>
-                <p className='text-sm'>Actuellement en reconversion professionnelle chez Simplon.co, je suis à la recherche d’un stage de deux mois</p>
-            </div>
+        <div className='max-w-[450px] flex flex-col gap-2 p-5'>
+            <p className='text-sm'>Nelli Timorshina</p>
+            <h1 className='uppercase font-bold'>Développeuse web</h1>
+            <p className='text-sm'>Actuellement en reconversion professionnelle chez Simplon.co, je suis à la recherche d’un stage de deux mois</p>
         </div>
-        <div className='flex flex-wrap mt-3'>
+        <div className='flex flex-wrap'>
             <div className='w-[250px]'>
-               <Contact />
+                <Contact />
                 <Skills />
             </div>
             <div>

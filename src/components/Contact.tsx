7b6c90fbcc6f520254f440dcc30869f7724d45a7
raftouch <ntimorshina@gmail.com>
2025-02-28 20:52:32 +0100

    updt contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export function Contact() {
   return (
     <div className="m-5 mb-4 pl-2 leading-9 text-sm">
       <p>&#9997; ntimorshina@gmail.com</p>
-      <p>&#10083; Paris, France</p>
     </div>
-  )
+  );
 }

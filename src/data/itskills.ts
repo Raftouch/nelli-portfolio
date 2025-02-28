51e2e138ef161900f4a84804aa1649b99f6fb8ee
raftouch <ntimorshina@gmail.com>
2025-02-28 20:36:29 +0100

    update it skills logos

--- src/data/itskills.ts
+++ src/data/itskills.ts
@@ -1,64 +1,74 @@
-import { MySkills } from '../models/MySkills'
+import { MySkills } from "../models/MySkills";
 
 export const itskills: MySkills[] = [
   {
     id: 1,
-    title: 'HTML5',
-    url: './logos/html.png',
+    title: "HTML5",
+    url: "./logos/html.png",
   },
   {
     id: 2,
-    title: 'CSS3',
-    url: './logos/css.png',
+    title: "CSS3",
+    url: "./logos/css.png",
   },
   {
     id: 3,
-    title: 'JavaScriptES6',
-    url: './logos/js.png',
+    title: "JavaScriptES6",
+    url: "./logos/js.png",
   },
   {
     id: 4,
-    title: 'TypeScript',
-    url: './logos/ts.png',
+    title: "TypeScript",
+    url: "./logos/ts.png",
   },
   {
     id: 5,
-    title: 'React18',
-    url: './logos/react.png',
+    title: "React18",
+    url: "./logos/react.png",
   },
   {
     id: 6,
-    title: 'Figma',
-    url: './logos/figma.png',
+    title: "Figma",
+    url: "./logos/figma.png",
   },
   {
     id: 7,
-    title: 'Git',
-    url: './logos/git.png',
+    title: "Git",
+    url: "./logos/git.png",
   },
   {
     id: 8,
-    title: 'WordPress',
-    url: './logos/wp.png',
+    title: "Docker",
+    url: "./logos/docker.webp",
   },
   {
     id: 9,
-    title: 'NodeJS',
-    url: './logos/nodejs.png',
+    title: "NodeJS",
+    url: "./logos/nodejs.png",
   },
   {
     id: 10,
-    title: 'MongoDB',
-    url: './logos/mongodb.png',
+    title: "MongoDB",
+    url: "./logos/mongodb.png",
   },
   {
     id: 11,
-    title: 'Bootstrap',
-    url: './logos/bootstrap.png',
+    title: "Python",
+    url: "./logos/python.png",
   },
   {
     id: 11,
-    title: 'Tailwind',
-    url: './logos/tailwind.png',
+    title: "Tailwind",
+    url: "./logos/tailwind.png",
   },
-]
+  {
+    id: 12,
+    title: "Postgresql",
+    url: "./logos/postgresql.webp",
+  },
+  {
+    id: 13,
+    title: "SQL",
+    url: "./logos/sql.png",
+  },
+];

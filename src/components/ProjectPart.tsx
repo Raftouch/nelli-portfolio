48a5f10d2473a8d1f99c64ef97bbe721a127cfd5
raftouch <rafanelkin@gmail.com>
2025-04-24 15:50:57 +0200

    fix padding

--- src/components/ProjectPart.tsx
+++ src/components/ProjectPart.tsx
@@ -5,7 +5,7 @@ import { ProjectCard } from "./ProjectCard";
 export function ProjectPart() {
   return (
     <>
-      <h1 className="font-serif text-center pt-48 text-3xl font-bold uppercase">
+      <h1 className="font-serif text-center pt-72 text-3xl font-bold uppercase">
         Projets récents
       </h1>
       <div className="min-h-full grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3 justify-items-center p-20">
